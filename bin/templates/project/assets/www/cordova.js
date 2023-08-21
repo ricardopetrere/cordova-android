9e85e5c05ef68f62e049b41ebb85a9ec784a0dbe
Ricardo Petrére <ricardo.petrere@etapaeducacional.onmicrosoft.com>
2023-08-21 18:52:29 -0300

    Atualizada versão do plugin Gradle para 4.0.0

--- bin/templates/project/assets/www/cordova.js
+++ bin/templates/project/assets/www/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '8.1.0';
+var PLATFORM_VERSION_BUILD_LABEL = '8.2.0';
 // file: src/scripts/require.js
 var require;
 var define;
